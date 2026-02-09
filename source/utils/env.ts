9c1fa27274f7ba9700933eb9da2e5fa35deccb34
Andrei Lineishchikov <andrei.l@fastmail.com>
2026-02-09 22:44:03 +0100

    env

---
+++ source/utils/env.ts
@@ -0,0 +1,16 @@
+if (
+  !import.meta.env.VITE_OTP_BUDDY_SAFARI_CLIENT_ID ||
+  !import.meta.env.VITE_OTP_BUDDY_WEB_CLIENT_ID ||
+  !import.meta.env.VITE_OTP_BUDDY_WEB_CLIENT_SECRET
+) {
+  console.log(import.meta.env);
+  throw new Error(
+    'VITE_OTP_BUDDY_SAFARI_CLIENT_ID, VITE_OTP_BUDDY_WEB_CLIENT_ID, and VITE_OTP_BUDDY_WEB_CLIENT_SECRET must be set'
+  );
+}
+
+export const env = {
+  OTP_BUDDY_SAFARI_CLIENT_ID: import.meta.env.VITE_OTP_BUDDY_SAFARI_CLIENT_ID,
+  OTP_BUDDY_WEB_CLIENT_ID: import.meta.env.VITE_OTP_BUDDY_WEB_CLIENT_ID,
+  OTP_BUDDY_WEB_CLIENT_SECRET: import.meta.env.VITE_OTP_BUDDY_WEB_CLIENT_SECRET,
+};
