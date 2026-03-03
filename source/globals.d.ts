9ec7f8518a07b36efdce4bfb6f23af5aa3035c80
Andrei Lineishchikov <andrei.l@fastmail.com>
2026-03-03 14:30:40 +0100

    use react@stable

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -1,6 +1,3 @@
-import {} from 'react/canary';
-import {} from 'react-dom/canary';
-
 // https://www.typescriptlang.org/tsconfig/#noUncheckedSideEffectImports
 declare module '*.scss' {
   const content: {[className: string]: string};
