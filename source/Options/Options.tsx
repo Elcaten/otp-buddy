fd63bea33dc811c0de8ddf838fdf1346dddd42a9
Andrei Lineishchikov <andrei.l@fastmail.com>
2026-03-07 00:12:17 +0100

    feat: implement Options layout with provider selection and Fastmail API key input

--- source/Options/Options.tsx
+++ source/Options/Options.tsx
@@ -1,25 +1,174 @@
-import type {FC} from 'react';
-import {useEffect, useState} from 'react';
-import {PROVIDERS} from '../types/providers';
-import {getStorage, setStorage} from '../utils/storage';
 import {JamClient} from 'jmap-jam';
+import {PropsWithChildren, ReactNode, Suspense, useState} from 'react';
+import {ErrorBoundary, FallbackProps as ErrorBoundaryFallbackProps} from 'react-error-boundary';
 import useSWR from 'swr';
-import {GmailOptions} from './gmail-options';
+import {Button} from '../components/ui/button';
+import {PROVIDERS} from '../types/providers';
+import {getAllStorage} from '../utils/storage';
 import {useDebounceState} from '../utils/use-debounce-state';
+import {GmailOptions} from './gmail-options';
 
-const Options: FC = () => {
-  const [provider, setProvider] = useState<'fastmail' | 'gmail'>('fastmail');
-  const [saved, setSaved] = useState(false);
+//#region Options layout
 
-  const [fastmailApiKey, setFastmailApiKey] = useDebounceState('', {
+const OptionsLayout = Object.assign(
+  function ({children}: PropsWithChildren) {
+    return <main className="mx-auto max-w-md p-4">{children}</main>;
+  },
+  {
+    Content: function Content({children}: PropsWithChildren) {
+      return <>{children}</>;
+    },
+    Form: function Form({children, onSubmit}: PropsWithChildren<{onSubmit?: React.FormEventHandler}>) {
+      return (
+        <form onSubmit={onSubmit}>
+          <h1>Email Provider settings</h1>
+          {children}
+        </form>
+      );
+    },
+  }
+);
+
+//#endregion
+
+//#region Form fields
+
+type ProverSelectorProps = {
+  value: 'fastmail' | 'gmail';
+  onChange?: (value: 'fastmail' | 'gmail') => void;
+};
+function ProviderSelector({value, onChange}: ProverSelectorProps) {
+  return (
+    <>
+      {Object.values(PROVIDERS).map((providerOption) => (
+        <div key={providerOption.id}>
+          <input
+            type="radio"
+            name="provider"
+            id={providerOption.id}
+            value={providerOption.id}
+            checked={value === providerOption.id}
+            onChange={(e): void => onChange?.(e.target.value as 'fastmail' | 'gmail')}
+          />
+          <label htmlFor={providerOption.id}>{providerOption.name}</label>
+        </div>
+      ))}
+    </>
+  );
+}
+
+type FastmailApiKeyInputProps = {
+  errorMessage?: ReactNode;
+  value: string;
+  onChange: (value: string) => void;
+};
+function FastmailApiKeyInput({errorMessage, value, onChange}: FastmailApiKeyInputProps) {
+  return (
+    <>
+      <label htmlFor="fastmailApiKey">Fastmail API key</label>
+      <div
+        style={{
+          fontSize: '0.85em',
+          color: 'var(--text-muted)',
+          marginBottom: '4px',
+        }}
+      >
+        You can generate API key in{' '}
+        <a href="https://app.fastmail.com/settings/security/tokens" target="_blank" rel="noopener noreferrer">
+          Fastmail settings
+        </a>
+        . Unfortunatelly, API keys are not available for Basic accounts.
+      </div>
+      <input
+        type="password"
+        id="fastmailApiKey"
+        name="fastmailApiKey"
+        placeholder="XXXXXXXX-XXXXXXXX-XXXXXXXX-XXXXXXXX"
+        spellCheck={false}
+        autoComplete="off"
+        value={value}
+        onChange={(e): void => onChange(e.target.value)}
+      />
+      {errorMessage && <div style={{color: 'var(--highlight)'}}>{errorMessage}</div>}
+      {!errorMessage && <div>&nbsp;</div>}
+    </>
+  );
+}
+
+type FastmailAccountsSelectorProps = {
+  isValidating: boolean;
+  options: {id: string; name: string}[];
+  value: string;
+  onChange: (value: string) => void;
+};
+function FastmailAccountsSelector({isValidating, options, value, onChange}: FastmailAccountsSelectorProps) {
+  return (
+    <>
+      <label htmlFor="accountId">
+        Account
+        {isValidating && <span aria-hidden>&nbsp;⏳</span>}
+      </label>
+      <select
+        id="accountId"
+        name="accountId"
+        disabled={isValidating}
+        value={value}
+        onChange={(e): void => onChange(e.target.value)}
+      >
+        <option value="">--- Select an account ---</option>
+        {options.map((account: {id: string; name: string}) => (
+          <option key={account.id} value={account.id}>
+            {account.name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+}
+//#endregion
+
+//#region Options states
+
+const OptionsState = {
+  Loading: () => (
+    <OptionsLayout>
+      <OptionsLayout.Content>
+        <OptionsLayout.Form>
+          <fieldset disabled>
+            <ProviderSelector value={'fastmail'} />
+          </fieldset>
+        </OptionsLayout.Form>
+      </OptionsLayout.Content>
+    </OptionsLayout>
+  ),
+  Error: (_props: ErrorBoundaryFallbackProps) => (
+    <OptionsLayout>
+      <OptionsLayout.Content>
+        <p>Something went wrong</p>
+      </OptionsLayout.Content>
+    </OptionsLayout>
+  ),
+};
+
+//#endregion
+
+//#region Container components
+
+function OptionsFormContainer(props: {
+  initialProvider: 'fastmail' | 'gmail';
+  initialFastmailApiKey: string;
+  initialFastmailAccountId: string;
+}) {
+  const [provider, setProvider] = useState(props.initialProvider);
+  const [fastmailApiKey, setFastmailApiKey] = useDebounceState(props.initialFastmailApiKey, {
     delay: 300,
     leading: true,
     trailing: true,
   });
-  const [fastmailAccountId, setFastmailAccountId] = useState('');
+  const [fastmailAccountId, setFastmailAccountId] = useState(props.initialFastmailAccountId);
+
   const accountQuery = useSWR(
     !!fastmailApiKey.debounced ? 'fastmailAccounts' + fastmailApiKey.debounced : null,
-
     async () => {
       const client = new JamClient({
         bearerToken: fastmailApiKey.debounced,
@@ -32,104 +181,72 @@ const Options: FC = () => {
     }
   );
 
-  useEffect(() => {
-    getStorage(['fastmailApiKey', 'fastmailAccountId', 'provider']).then((result) => {
-      setFastmailApiKey(result.fastmailApiKey);
-      setFastmailAccountId(result.fastmailAccountId);
-      setProvider(result.provider);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const handleFastmailApiKeyChange = (value: string) => {
+    setFastmailApiKey(value);
+  };
 
-  const handleSave = async (e: React.FormEvent): Promise<void> => {
-    e.preventDefault();
-    await setStorage({
-      fastmailApiKey: fastmailApiKey.debounced,
-      fastmailAccountId,
-      provider,
-    });
-    setSaved(true);
-    setTimeout(() => setSaved(false), 2000);
+  const handleFastmailAccountIdChange = (value: string) => {
+    setFastmailAccountId(value);
   };
 
   return (
-    <main className="mx-auto max-w-md p-4">
-      <form onSubmit={handleSave}>
-        <h1>Email Provider settings</h1>
-
-        <fieldset>
-          {Object.values(PROVIDERS).map((providerOption) => (
-            <div key={providerOption.id}>
-              <input
-                type="radio"
-                name="provider"
-                id={providerOption.id}
-                value={providerOption.id}
-                checked={provider === providerOption.id}
-                onChange={(e): void => setProvider(e.target.value as 'fastmail' | 'gmail')}
-              />
-              <label htmlFor={providerOption.id}>{providerOption.name}</label>
-            </div>
-          ))}
-        </fieldset>
-
-        {provider === 'gmail' && <GmailOptions />}
-
-        {provider === 'fastmail' && (
+    <OptionsLayout>
+      <OptionsLayout.Content>
+        <OptionsLayout.Form>
           <fieldset>
-            <label htmlFor="fastmailApiKey">Fastmail API key</label>
-            <div
-              style={{
-                fontSize: '0.85em',
-                color: 'var(--text-muted)',
-                marginBottom: '4px',
-              }}
-            >
-              You can generate API key in{' '}
-              <a href="https://app.fastmail.com/settings/security/tokens" target="_blank" rel="noopener noreferrer">
-                Fastmail settings
-              </a>
-              . Unfortunatelly, API keys are not available for Basic accounts.
-            </div>
-            <input
-              type="password"
-              id="fastmailApiKey"
-              name="fastmailApiKey"
-              placeholder="XXXXXXXX-XXXXXXXX-XXXXXXXX-XXXXXXXX"
-              spellCheck={false}
-              autoComplete="off"
-              value={fastmailApiKey.value}
-              onChange={(e): void => setFastmailApiKey(e.target.value)}
-            />
-            {Boolean(accountQuery.isValidating) && <div>⏳</div>}
-            {Boolean(accountQuery.error) && (
-              <div style={{color: 'var(--highlight)'}}>Can&apos;t connect to Fastmail. Please check your API key.</div>
-            )}
-            {!Boolean(accountQuery.isValidating) && !Boolean(accountQuery.error) && <div>&nbsp;</div>}
-
-            <label htmlFor="accountId">Account</label>
-            <select
-              id="accountId"
-              name="accountId"
-              disabled={accountQuery.isValidating || !accountQuery.data}
-              value={fastmailAccountId}
-              onChange={(e): void => setFastmailAccountId(e.target.value)}
-            >
-              <option value="">--- Select an account ---</option>
-              {accountQuery.data?.map((account: {id: string; name: string}) => (
-                <option key={account.id} value={account.id}>
-                  {account.name}
-                </option>
-              ))}
-            </select>
+            <ProviderSelector value={provider} onChange={setProvider} />
           </fieldset>
-        )}
 
-        <button type="submit">Save Settings</button>
-        {saved && <span style={{color: 'var(--highlight)', marginLeft: '8px'}}>Settings saved!</span>}
-      </form>
-    </main>
+          {provider === 'gmail' && <GmailOptions />}
+
+          {provider === 'fastmail' && (
+            <fieldset>
+              <FastmailApiKeyInput
+                errorMessage={accountQuery.error ? `Can't connect to Fastmail. Please check your API key.` : undefined}
+                value={fastmailApiKey.value}
+                onChange={handleFastmailApiKeyChange}
+              />
+
+              <FastmailAccountsSelector
+                value={fastmailAccountId}
+                onChange={handleFastmailAccountIdChange}
+                isValidating={accountQuery.isValidating}
+                options={accountQuery.data ?? []}
+              />
+            </fieldset>
+          )}
+
+          <Button type="submit">Save</Button>
+        </OptionsLayout.Form>
+      </OptionsLayout.Content>
+    </OptionsLayout>
   );
-};
+}
 
-export default Options;
+function OptionsContentContainer() {
+  const storageQuery = useSWR('storage', () => getAllStorage(), {suspense: true});
+
+  return (
+    <OptionsFormContainer
+      initialProvider={storageQuery.data.provider}
+      initialFastmailApiKey={storageQuery.data.fastmailApiKey}
+      initialFastmailAccountId={storageQuery.data.fastmailAccountId}
+    />
+  );
+}
+
+//#endregion
+
+//#region Options itself
+
+export default function Options() {
+  return (
+    <ErrorBoundary FallbackComponent={OptionsState.Error}>
+      <Suspense fallback={<OptionsState.Loading />}>
+        <OptionsContentContainer />
+      </Suspense>
+    </ErrorBoundary>
+  );
+}
+
+//#endregion
