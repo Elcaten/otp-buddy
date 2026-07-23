34816391c9305a653a2560f704e1666234efd897
Andrei Lineishchikov <andrei.l@fastmail.com>
2026-07-23 14:03:38 +0200

    remove debugger

--- source/email-parser/email-parser.ts
+++ source/email-parser/email-parser.ts
@@ -20,7 +20,6 @@ export class EmailParser implements IEmailParser {
   }
 
   tryParse(email: Email): ParseResult {
-    debugger;
     const rule = this.config.rules.find((r) => ruleMatches(r, email));
     if (!rule) return {success: false, error: 'not-found'};
     for (const extractor of rule.extractors) {
